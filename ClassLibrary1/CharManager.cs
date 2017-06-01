3c462e67ef92e96ffc88357c22e2d97dd59f58c3
guswadsworth12 <guswadsworth12@gmail.com>
2017-06-01 05:02:15 -0600

    Started the Character manager
    Created the character manager file.

---
+++ ClassLibrary1/CharManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GusWadsworth12.VPTU.CharManager
+{
+    public class CharManager
+    {
+        // This will be the Character Sheet manager
+         
+
+    }
+}
